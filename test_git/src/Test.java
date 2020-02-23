0243ac7d25793992d69269cac390978379fe8d05
hqc <1124102102@qq.com>
2020-02-23 20:11:00 +0800

    提交test_git工程到本地仓库

---
+++ test_git/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("gitttttttttttttttttt");
+        System.out.println("gitttttttttttttttttt");
+        System.out.println("gitttttttttttttttttt");
+        System.out.println("gitttttttttttttttttt");
+        System.out.println("gitttttttttttttttttt");
+    }
+}
